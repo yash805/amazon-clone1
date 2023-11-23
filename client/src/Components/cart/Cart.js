628312b9a150040510189bf5304363674711a13f
Tejasreep <36584960+Tejasreep@users.noreply.github.com>
2023-11-23 21:22:25 +0530

    Update Cart.js

--- client/src/Components/cart/Cart.js
+++ client/src/Components/cart/Cart.js
@@ -23,7 +23,7 @@ const Cart = () => {
     console.log(inddata);
 
     const getinddata = async () => {
-        const res = await fetch(`/getproductsone/${id}`, {
+        const res = await fetch(`https://amazon-clone-api-sooty.vercel.app/getproductsone/${id}`, {
             method: "GET",
             headers: {
                 Accept: "application/json",
