3a981b45cd746738faf882a1355b8e326bab104c
Tejasreep <36584960+Tejasreep@users.noreply.github.com>
2023-11-24 21:56:09 +0530

    Update Buynow.js

--- client/src/Components/buynow/Buynow.js
+++ client/src/Components/buynow/Buynow.js
@@ -29,7 +29,7 @@ const Buynow = () => {
       console.log("no data available")
     } else {
       // console.log("data cart main hain");
-      setCartdata(data?.carts.filter((cart)=> cart !== null));
+      setCartdata(data?.carts.filter((cart)=> cart && cart !== null));
     }
   };
 
