98ec2c573bdfa546607daaa19d0c8844b38908b4
Tejasreep <36584960+Tejasreep@users.noreply.github.com>
2023-11-24 13:15:06 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const router = require("./routes/router");
 
 app.use(express.json());
 app.use(cookieParser("your-secret-key"));
-app.use(cors({ origin: ['https://amazon-clone-client-ten.vercel.app', 'http://localhost:3000'] , credentials :  true, methods: ["POST", "GET", "PUT", "DELETE"], allowedHeaders: ["Cookie, Content-Type, Accept, x-requested-with, Cache-Control"]}));
+app.use(cors({ origin: ['https://amazon-clone-client-ten.vercel.app', 'http://localhost:3000'] , credentials :  true, methods: ["POST", "GET", "PUT", "DELETE"]}));
 app.use(router);
 
 
