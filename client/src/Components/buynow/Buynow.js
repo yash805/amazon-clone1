6ce52de28bfbeffb945f8d1b177c5f9ac2d15890
yash805 <74845238+yash805@users.noreply.github.com>
2023-12-04 16:40:27 +0530

    Update Buynow.js

--- client/src/Components/buynow/Buynow.js
+++ client/src/Components/buynow/Buynow.js
@@ -19,7 +19,7 @@ const Buynow = () => {
   };
   
   const getdatabuy = async () => {
-    const res = await fetch("https://amazon-clone-api-two.vercel.app/cartdetails", {
+    const res = await fetch("hhttps://amazon-clone1-back.vercel.app/cartdetails", {
       method: "GET",
       headers: {
         Accept: "application/json",
