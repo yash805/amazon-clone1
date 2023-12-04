93c12a615fcf6936b893a66c40f85cf90d731719
yash805 <74845238+yash805@users.noreply.github.com>
2023-12-04 19:29:41 +0530

    Update Right.js

--- client/src/Components/buynow/Right.js
+++ client/src/Components/buynow/Right.js
@@ -11,7 +11,7 @@ const Right = ({iteam, quantityMap}) => {
   
     const totalAmount = () => {
         let price = 0
-        iteam?.map((item)=>{
+        iteam.map((item)=>{
             const quantity = quantityMap[item.id] || 1; // Default quantity is 1
             price += item.price.cost * quantity;
     });
@@ -23,7 +23,7 @@ const Right = ({iteam, quantityMap}) => {
     <div className="cost_right">
         <p>Your order is eligible for FREE Delivery. <br />
             <span style={{ color: "#565959" }}> Select this option at checkout. Details</span></p>
-        <h3>Subtotal ({iteam?.length} items): <span style={{ fontWeight: "700" }}> ₹{price}.00</span></h3>
+        <h3>Subtotal ({iteam.length} items): <span style={{ fontWeight: "700" }}> ₹{price}.00</span></h3>
         <button className="rightbuy_btn" >Proceed to Buy</button>
         <div className="emi">
             Emi available
