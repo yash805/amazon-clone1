1617366a3b5184f5e18f9b52182d6eeb1868657d
yash805 <74845238+yash805@users.noreply.github.com>
2023-12-04 13:21:04 +0530

    Update Newnav.js

--- client/src/Components/newnavbaar/Newnav.js
+++ client/src/Components/newnavbaar/Newnav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink, useNavigate } from 'react-router-dom';
 import "./newnav.css"; 
 
 const Newnav = () => {
@@ -7,7 +8,7 @@ const Newnav = () => {
         <div className="nav_data">
             <div className="left_data">
                 <p>All</p>
-                <p>Mobile</p>
+                <NavLink to="/mobile"><p>Mobile</p></NavLink>
                 <p>Bestseller</p>
                 <p>Fashion</p>
                 <p>Customer Services</p>
