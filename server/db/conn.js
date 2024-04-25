07d9df3da0628d6918b076c67df944dbf2b8cadd
Tejasreep <36584960+Tejasreep@users.noreply.github.com>
2024-04-25 21:20:19 +0530

    Delete server directory

--- server/db/conn.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose");
-const DB = process.env.DATABASE;
-
-mongoose.connect(DB).then(()=>console.log("data base connected")).catch((error)=>console.log("errors" + error.message));
